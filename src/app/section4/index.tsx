5ae60ab2f66432f644a3acef99a312c6cbe6e4e7
honghav <leanghonghav1682gmail.com>
2026-08-17 17:06:56 +0700

    Setup Module

---
+++ src/app/section4/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Section4() {
+
+    return (
+        <View>
+            <Text>Section 4</Text>
+        </View>
+    );
+}
--- src/model/module/module.data.ts
+++ src/model/module/module.data.ts
@@ -10,10 +10,24 @@ export const moduleData: ModuleDTO[] = [
     },
     {
         id: '2',
-        name: 'Section 2: React Native Project 1 Basic Project',
+        name: 'Section 2: React Native Project 1 Basic Project: Expense Tracker',
         path: '/section2',
         icon: '💡',
         description: 'Section 2 description',
+    },
+    {
+        id: '3',
+        name: 'Section 3: React Native Project Advanced Project: Phone Permission',
+        path: '/section3',
+        icon: '💡',
+        description: 'Section 3 description',
+    },
+    {
+        id: '4',
+        name: 'Section 4: React Native Project Advanced Project: Webhook Project',
+        path: '/section4',
+        icon: '💡',
+        description: 'Section 4 description',
     },
 ];
 
